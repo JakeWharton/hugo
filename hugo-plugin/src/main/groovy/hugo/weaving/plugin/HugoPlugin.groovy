47146ce9e76011278f88b2da22266710820fd9da
Jake Wharton <jw@squareup.com>
2013-11-25 01:44:22 -0800

    Send AspectJ logs to the right place.

--- hugo-plugin/src/main/groovy/hugo/weaving/plugin/HugoPlugin.groovy
+++ hugo-plugin/src/main/groovy/hugo/weaving/plugin/HugoPlugin.groovy
@@ -3,6 +3,8 @@ package hugo.weaving.plugin
 import com.android.build.gradle.AppPlugin
 import com.android.build.gradle.LibraryPlugin
 import com.android.builder.BuilderConstants
+import org.aspectj.bridge.IMessage
+import org.aspectj.bridge.MessageHandler
 import org.aspectj.tools.ajc.Main
 import org.gradle.api.Plugin
 import org.gradle.api.Project
@@ -48,7 +50,26 @@ class HugoPlugin implements Plugin<Project> {
         ]
         log.debug "ajc args: " + Arrays.toString(args)
 
-        new Main().runMain(args, false)
+        MessageHandler handler = new MessageHandler(true);
+        new Main().run(args, handler);
+        for (IMessage message : handler.getMessages(null, true)) {
+          switch (message.getKind()) {
+            case IMessage.ABORT:
+            case IMessage.ERROR:
+            case IMessage.FAIL:
+              log.error message.message, message.thrown
+              break;
+            case IMessage.WARNING:
+              log.warn message.message, message.thrown
+              break;
+            case IMessage.INFO:
+              log.info message.message, message.thrown
+              break;
+            case IMessage.DEBUG:
+              log.debug message.message, message.thrown
+              break;
+          }
+        }
       }
     }
   }
