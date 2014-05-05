489c9d125563ac5435ef19fa06317be14b810158
Jake Wharton <jw@squareup.com>
2014-05-05 00:01:05 -0700

    Prepare next development version.

--- hugo-plugin/src/main/groovy/hugo/weaving/plugin/HugoPlugin.groovy
+++ hugo-plugin/src/main/groovy/hugo/weaving/plugin/HugoPlugin.groovy
@@ -26,10 +26,10 @@ class HugoPlugin implements Plugin<Project> {
     }
 
     project.dependencies {
-      debugCompile 'com.jakewharton.hugo:hugo-runtime:1.1.0'
+      debugCompile 'com.jakewharton.hugo:hugo-runtime:1.1.1-SNAPSHOT'
       // TODO this should come transitively
       debugCompile 'org.aspectj:aspectjrt:1.7.4'
-      compile 'com.jakewharton.hugo:hugo-annotations:1.1.0'
+      compile 'com.jakewharton.hugo:hugo-annotations:1.1.1-SNAPSHOT'
     }
 
     variants.all { variant ->
