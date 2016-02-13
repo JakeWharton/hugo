0acc737258f79c2624a8d12305b27c1121c26c0a
William Webb <jug6ernaut@gmail.com>
2016-02-13 17:50:19 -0600

    Lower retention level for  @Profile, not needed at runtime.

--- hugo-annotations/src/main/java/hugo/weaving/Profile.java
+++ hugo-annotations/src/main/java/hugo/weaving/Profile.java
@@ -6,13 +6,13 @@ import java.lang.annotation.Target;
 import static java.lang.annotation.ElementType.CONSTRUCTOR;
 import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.ElementType.TYPE;
-import static java.lang.annotation.RetentionPolicy.RUNTIME;
+import static java.lang.annotation.RetentionPolicy.CLASS;
 
 /**
  * Created by williamwebb on 2/10/16.
  */
 
 
-@Target({TYPE, METHOD, CONSTRUCTOR}) @Retention(RUNTIME)
+@Target({TYPE, METHOD, CONSTRUCTOR}) @Retention(CLASS)
 public @interface Profile {
 }
